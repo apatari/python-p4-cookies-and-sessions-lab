5f071242e560d29e077ec119d70d654b99d6a911
Andy Patari <andy.patari@gmail.com>
2023-11-06 16:42:26 -0500

    passing tests

--- server/app.py
+++ server/app.py
@@ -28,7 +28,17 @@ def index_articles():
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session["page_views"] = session.get("page_views") or 0
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        article_data = Article.query.filter_by(id=id).first().to_dict()
+        response = make_response(article_data, 200)
+        return response
+    else:
+        body = {"message": "Maximum pageview limit reached"}
+        response = make_response(body, 401)
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
